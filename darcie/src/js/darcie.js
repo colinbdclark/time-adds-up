2bd72ff6720bd717b9a4f4c26f66126c28d8bbbd
Colin Clark <colinbdclark@gmail.com>
2018-09-09 18:33:43 -0400

    Adds videos for Darcie and Cookie.

--- darcie/src/js/darcie.js
+++ darcie/src/js/darcie.js
@@ -2,7 +2,6 @@ fluid.defaults("timeAddsUp.darcie.compositor", {
     gradeNames: "timeAddsUp.compositor",
 
     layerURLs: [
-        "videos/1080x720/1.m4v",
         "videos/1080x720/2.m4v",
         "videos/1080x720/3.m4v",
         "videos/1080x720/4.m4v",
@@ -12,7 +11,16 @@ fluid.defaults("timeAddsUp.darcie.compositor", {
 
     layerSpecs: [
         {
+            url: "{that}.options.layerURLs.0"
+        },
+        {
             url: "{that}.options.layerURLs.1"
+        },
+        {
+            url: "{that}.options.layerURLs.1",
+            clip: {
+                inTime: 20
+            }
         },
         {
             url: "{that}.options.layerURLs.2"
@@ -20,50 +28,41 @@ fluid.defaults("timeAddsUp.darcie.compositor", {
         {
             url: "{that}.options.layerURLs.2",
             clip: {
-                inTime: 20
+                inTime: 25
             }
         },
         {
             url: "{that}.options.layerURLs.3"
         },
         {
-            url: "{that}.options.layerURLs.3",
-            clip: {
-                inTime: 25
-            }
-        },
-        {
             url: "{that}.options.layerURLs.4"
         },
         {
-            url: "{that}.options.layerURLs.5"
-        },
-        {
-            url: "{that}.options.layerURLs.5",
+            url: "{that}.options.layerURLs.4",
             clip: {
                 inTime: 3
             }
         },
         {
-            url: "{that}.options.layerURLs.5",
+            url: "{that}.options.layerURLs.4",
             clip: {
                 inTime: 7
             }
         },
         {
-            url: "{that}.options.layerURLs.5",
+            url: "{that}.options.layerURLs.4",
             clip: {
                 inTime: 10
             }
         },
         {
-            url: "{that}.options.layerURLs.5",
+            url: "{that}.options.layerURLs.4",
             clip: {
                 inTime: 21
             }
         },
         {
-            url: "{that}.options.layerURLs.5",
+            url: "{that}.options.layerURLs.4",
             clip: {
                 inTime: 30,
                 outTime: 69
